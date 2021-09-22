ae3ed4f8e43f2b44f72917abc0872d8437fc097a
Ramesh yanith <yanithramesh77@gmail,com>
2021-09-22 23:40:09 +0530

    initial commit

---
+++ lib/register.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        height: size.height,
+        color: Color(0xffE5E5E5),
+      ),
+    );
+  }
+}
