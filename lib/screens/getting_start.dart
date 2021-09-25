ae222882dae7af034510a4e22ce56ef1e1eb0434
Ramesh yanith <yanithramesh77@gmail,com>
2021-09-26 01:22:56 +0530

    splash screen added

---
+++ lib/screens/getting_start.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Gettingstart extends StatefulWidget {
+  const Gettingstart({Key? key}) : super(key: key);
+
+  @override
+  _GettingstartState createState() => _GettingstartState();
+}
+
+class _GettingstartState extends State<Gettingstart> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
---
+++ lib/screens/splash_screen/splash_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:food/screens/getting_start.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 5), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => Gettingstart()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        height: size.height,
+        width: size.width,
+        color: Color(0xE5E5E5),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("Assest/images/food.png"),
+            const SizedBox(height: 8),
+            const Text(
+              "No waiting for food",
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 14,
+                color: Color(0xff838383),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
